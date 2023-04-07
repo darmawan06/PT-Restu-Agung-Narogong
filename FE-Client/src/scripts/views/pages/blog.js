4ff3d5ccc477b803b8bad2344b5b0d23a927ea87
- <71787768+AmarMsd@users.noreply.github.com>
2023-04-07 07:39:55 +0700

    Update Section Karir and Blog

--- FE-Client/src/scripts/views/pages/blog.js
+++ FE-Client/src/scripts/views/pages/blog.js
@@ -1,6 +1,6 @@
 const blog = {
-	init(){
-		return `
+  init() {
+    return `
 			<section class="w-full h-[300px] bg-gray-50 flex flex-col px-[8%]">
 		      <div class="w-full m-auto flex-col">
 		        <p class="text-[50px] font-primary text-white text-center tracking-wider text-black">Blog</p>
@@ -23,12 +23,196 @@ const blog = {
 		        <p class="my-auto text-sm">Blog</p>
 		      </a>
 		    </section>
-		`;
-	},
+			<section class="w-full flex flex-col px-[8%]">
+      <div class="md:flex md:flex-row flex flex-col mt-12">
+        <div class="md:w-[75%]">
+          <div class="md:p-12 p-4 bg-gray-50">
+            <img class="m-auto blur-sm" src="blog-image.jpg" alt="Blog Image" width="848" height="300" />
+            <div class="mt-8 text-gray-500">
+              <time datetime="2023-10-12">October 12, 2023 &#124;</time>
+              <span>By James &#124;</span>
+              <span>
+                <a href="#">Construction</a>
+                •
+                <a href="#">General Information &#124; </a>
+              </span>
+              <span>
+                <a href="#">0 Comment</a>
+              </span>
+            </div>
+            <div class="">
+              <h2 class="text-3xl font-bold mt-4">Tips And Tricks</h2>
+              <div class="border relative my-3 w-full">
+                <div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600"></div>
+              </div>
+              <p class="text-gray-800">
+                If you are looking to shift to a new house or want to renovate your existing one, the first thing you will need to think about is the painting. While it may be tempting to hire a professional to do it if you have never
+                painted before, it will cost you a lot of money. Painting your own house allows you to experiment with something new and will help you save thousands of dollars.
+              </p>
+              <br />
+              <a href="#" class="font-semibold">READ MORE</a>
+            </div>
+          </div>
 
-	afterRender(){
-		return "";
-	}
-}
+          <div class="md:p-12 mt-8">
+            <img class="m-auto" src="blog-image.jpg" alt="Blog Image" width="848" height="300" />
+            <div class="mt-8 text-gray-500">
+              <time datetime="2023-10-12">October 12, 2023 &#124;</time>
+              <span>By James &#124;</span>
+              <span>
+                <a href="#">Construction</a>
+                •
+                <a href="#">General Information &#124; </a>
+              </span>
+              <span>
+                <a href="#">0 Comment</a>
+              </span>
+            </div>
+            <div class="">
+              <h2 class="text-3xl font-bold mt-4">Tips And Tricks</h2>
+              <div class="border relative my-3 w-full">
+                <div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600"></div>
+              </div>
+              <p class="text-gray-800">
+                If you are looking to shift to a new house or want to renovate your existing one, the first thing you will need to think about is the painting. While it may be tempting to hire a professional to do it if you have never
+                painted before, it will cost you a lot of money. Painting your own house allows you to experiment with something new and will help you save thousands of dollars.
+              </p>
+              <br />
+              <a href="#" class="font-semibold">READ MORE</a>
+            </div>
+          </div>
+
+          <div class="md:p-12 mt-8">
+            <img class="m-auto" src="blog-image.jpg" alt="Blog Image" width="848" height="300" />
+            <div class="mt-8 text-gray-500">
+              <time datetime="2023-10-12">October 12, 2023 &#124;</time>
+              <span>By James &#124;</span>
+              <span>
+                <a href="#">Construction</a>
+                •
+                <a href="#">General Information &#124; </a>
+              </span>
+              <span>
+                <a href="#">0 Comment</a>
+              </span>
+            </div>
+            <div class="">
+              <h2 class="text-3xl font-bold mt-4">Tips And Tricks</h2>
+              <div class="border relative my-3 w-full">
+                <div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600"></div>
+              </div>
+              <p class="text-gray-800">
+                If you are looking to shift to a new house or want to renovate your existing one, the first thing you will need to think about is the painting. While it may be tempting to hire a professional to do it if you have never
+                painted before, it will cost you a lot of money. Painting your own house allows you to experiment with something new and will help you save thousands of dollars.
+              </p>
+              <br />
+              <a href="#" class="font-semibold">READ MORE</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="md:w-[25%] md:ml-8">
+          <form action="" class="relative">
+            <input type="search" id="default-search" class="block md:w-[15vw] w-full p-2 pl-3 text-sm text-gray-900 border border-gray-300" placeholder="Search ..." />
+            <button type="submit" class="text-white absolute right-0 md:w-[5vw] p-2 top-0 bg-red-600 h-full focus:ring-blue-300 font-medium">Search</button>
+          </form>
+          <div class="mt-4">
+            <p class="font-semibold">Recent Posts</p>
+            <div class="border relative my-3 w-full">
+              <div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600"></div>
+            </div>
+            <ul class="list-disc ml-8">
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+            </ul>
+          </div>
+
+          <div class="mt-4">
+            <p class="font-semibold">Recent Comments</p>
+            <div class="border relative my-3 w-full">
+              <div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600"></div>
+            </div>
+            <ul class="list-disc ml-8">
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+            </ul>
+          </div>
+
+          <div class="mt-4">
+            <p class="font-semibold">Archive</p>
+            <div class="border relative my-3 w-full">
+              <div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600"></div>
+            </div>
+            <ul class="list-disc ml-8">
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+            </ul>
+          </div>
+
+          <div class="mt-4">
+            <p class="font-semibold">Categories</p>
+            <div class="border relative my-3 w-full">
+              <div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600"></div>
+            </div>
+            <ul class="list-disc ml-8">
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+            </ul>
+          </div>
+
+          <div class="mt-4">
+            <p class="font-semibold">Meta</p>
+            <div class="border relative my-3 w-full">
+              <div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600"></div>
+            </div>
+            <ul class="list-disc ml-8">
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+            </ul>
+          </div>
+
+          <div class="mt-4">
+            <p class="font-semibold">Awesome Posts</p>
+            <div class="border relative my-3 w-full">
+              <div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600"></div>
+            </div>
+            <ul class="list-disc ml-8">
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+              <a href="#"><li class="underline-offset-auto hover:underline-offset-2">Hello world!</li></a>
+            </ul>
+          </div>
+
+          <div class="mt-4 w-full">
+            <p class="font-semibold">Tag Cloud</p>
+            <div class="border relative my-3 w-full">
+              <div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600"></div>
+            </div>
+            <div class="flex flex-wrap">
+              <br />
+              <a href="#" class="bg-red-600 h-full text-white ml-2 p-1">Construction</a>
+              <a href="#" class="bg-red-600 h-full text-white ml-2 p-1">Construction</a>
+              <a href="#" class="bg-red-600 h-full text-white ml-2 p-1">Construction</a>
+              <a href="#" class="bg-red-600 h-full text-white ml-2 mt-2 p-1">Construction</a>
+              <a href="#" class="bg-red-600 h-full text-white ml-2 mt-2 p-1">Construction</a>
+              <a href="#" class="bg-red-600 h-full text-white ml-2 mt-2 p-1">Construction</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+		`;
+  },
+
+  afterRender() {
+    return "";
+  },
+};
 
 export default blog;
--- FE-Client/src/scripts/views/pages/karir.js
+++ FE-Client/src/scripts/views/pages/karir.js
@@ -1,6 +1,6 @@
 const karir = {
-	init(){
-		return `
+  init() {
+    return `
 			<section class="w-full h-[300px] bg-gray-50 flex flex-col px-[8%]">
 		      <div class="w-full m-auto flex-col">
 		        <p class="text-[50px] font-primary text-white text-center tracking-wider text-black">Karir</p>
@@ -23,15 +23,104 @@ const karir = {
 		        <p class="my-auto text-sm">Karir</p>
 		      </a>
 		    </section>
-		    <section>
-		    	Hahahaha
-		    </section>
-		`;
-	},
+		    <section class="w-full bg-gray-50 flex flex-col px-[8%] ">
+				<p class="mt-12">Lowongan diperusahan kami:</p>
 
-	afterRender(){
-		return "";
-	}
-}
+				<div class="md:w-[50vw] mt-8">
+				<p class="text-4xl font-bold">Pelaksaan Lapangan</p>
+
+				<div class="border relative my-3 w-full">
+					<div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600 "></div>
+				</div>
+
+					<p class="">Pesyaratan :</p>
+					
+					<ul class="list-disc ml-12 ml-12">
+						<li>Pendidikan minimal D3 Teknik Sipil / STM Bangunan</li>
+						<li>Pengalaman minimal 3 tahun</li>
+						<li>Menguasai sistem manajemen mutu, standar toleransi, dan instruksi kerja di proyek. Diutamakan yang menguasai estimasi volume.</li>
+						<li>Mampu bekerja di bawah tekanan / tight schedule</li>
+						<li>Siap bekerja lembur</li>
+					<ul>
+					
+				</div>
+
+
+				<div class="md:w-[50vw] mt-8">
+					
+					<h1 class="text-4xl font-bold">Logistik</h1>
+
+					<div class="border relative my-3 w-full">
+						<div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600 "></div>
+					</div>
+
+					<p class="">Pesyaratan :</p>
+					<ul class="list-disc  ml-12">
+						<li>Pendidikan minimal D3 Teknik Sipil / Elektro / Mesin atau STM</li>
+						<li>Pengalaman sebagai Staff Logistik Gudang lebih diutamakan</li>
+						<li>Memiliki pemahaman yang baik mengenai material dan peralatan konstruksi bangunan</li>
+						<li>Menguasai / mahir menggunakan komputer (Ms. Office)</li>
+						<li>Siap bekerja lembur jika diperlukan</li>
+					<ul>
+				</div>
+
+
+				<div class="md:w-[50vw] mt-8">
+					<h1 class="text-4xl font-bold">Staff Personalia</h1>
+
+					<div class="border relative my-3 w-full">
+						<div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600 "></div>
+					</div>
+
+					<p class="">Pesyaratan :</p>
+					<ul class="list-disc  ml-12">
+						<li>Diutamakan wanita</li>
+						<li>Pendidikan minimal D3/S1 Hukum atau Psikologi atau sejenisnya dari universitas dengan akreditasi A atau B pada jurusan tersebut</li>
+						<li>Pengalaman di bidang hukum atau personalia lebih diutamakan</li>
+						<li>Pemahaman yang baik mengenai bahasa hukum dan undang-undang</li>
+						<li>Mampu bersikap tegas, terampil bernegosiasi dan komunikatif</li>
+					<ul>
+
+				</div>
+
+
+				<div class="md:w-[50vw] mt-8">
+					<h1 class="text-4xl font-bold">Mechanical Electrical</h1>
+
+					<div class="border relative my-3 w-full">
+						<div class="border-2 absolute md:w-[2vw] w-[10vw] bottom-0 border-red-600 "></div>
+					</div>
+
+					<p class="">Pesyaratan :</p>
+					<ul class="list-disc  ml-12">
+						<li>Pendidikan minimal D3 Elektronik atau setara</li>
+						<li>Pengalaman minimal 3 tahun</li>
+						<li>Mampu bekerja di bawah tekanan / tight schedule</li>
+					<ul>
+					
+				</div>
+				<div class="md:w-[50vw] mt-8">
+
+					<p>Mohon mengirimkan lamaran ke hrd@purisuma.id atau mengirimkan langsung ke alamat kantor kami. Terima kasih.</p>
+					<br>
+
+					<p>
+						NB: PT. PURI SUMA MANDIRI tidak pernah memungut biaya apapun untuk wawancara dan tidak bekerjasama dengan pihak
+						ketiga vendor yang mengatasnamakan PT. PURI SUMA MANDIRI untuk melakukan panggilan wawancara. 
+						Semua panggilan akan melalui email resmi kami hrd@purisuma.id.
+					</p>
+				</div>
+				<br>
+
+
+			</section>
+
+		`;
+  },
+
+  afterRender() {
+    return "";
+  },
+};
 
 export default karir;
