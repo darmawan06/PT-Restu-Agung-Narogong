69fb9a03804bbacc137b5ce7a2639ca1f253e547
- <71787768+AmarMsd@users.noreply.github.com>
2023-04-14 02:21:09 +0700

    Update Layanan Kami

--- FE-Client/src/scripts/views/pages/layanan_kami.js
+++ FE-Client/src/scripts/views/pages/layanan_kami.js
@@ -197,81 +197,54 @@ const layanan_kami = {
 					</div>
 				</div>
 
-				<div class="show_popup_layanan absolute lg:right-36 drop-shadow-xl bg-gray-100 z-[1000] min-h-screen">
+				<div class="show_popup_layanan fixed w-full left-0 top-0 drop-shadow-xl bg-gray-100 z-[1000] h-[100vh]">
 				<button class="close_popup absolute top-2 right-2 bg-red-600 text-white px-3 py-1 font-semibold">X</button>
-					<div class="lg:w-[80vw] md:w-[84vw] w-full text-justify ">
-						<div class="lg:flex lg:flex-col lg:m-24 lg:p-0 p-4 mt-8 md:p-8">
-							<img class="lg:w-[30vw] md:mx-auto" src="images/proyek (1).jpg" width="500" alt="Pop Up Image">
-							<div class="mt-8">
-								<p class="font-bold font-primary text-xl">
+					<div class="w-full h-full text-justify overflow-y-scroll">
+						<div class="lg:flex lg:flex-col lg:m-24 lg:p-0 p-4 mt-8 md:p-8 text-black">
+							<p class="font-semibold font-secondary text-[35px] text-center">
 								Lorem ipsum dolor sit amet,  
-								</p>
-								<p class="mt-4">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus.
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-
-								</p>
-								<br>
-								<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus.
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-
-								</p>
-								<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus.
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-
-								</p>
-								<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus.
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-
-								</p>
-								<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus.
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-
-								</p>
-								<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus.
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-
-								</p>
-								<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus.
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
-									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
-
-								</p>
-
+							</p>
+							<div class="w-full h-[2px] relative my-4">
+								<div class="w-[100px] absolute mx-auto h-full bg-red-600 z-10 top-0 left-0 right-0"></div>
+								<div class="w-full absolute mx-auto h-full bg-gray-200 top-0 left-0 right-0"></div>
 							</div>
+
+							<img class="lg:w-[80%] lg:h-[70vh] object-cover md:mx-auto rounded-sm shadow-md" src="images/proyek (1).jpg" width="500" alt="Pop Up Image">
+							
+							<div class="mt-8 lg:w-full px-[12%] ">
+								<p class="mt-4 lg:w-full whitespace-pre-line text-[18px] font-secondary text-justify">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at
+									quam ut purus pulvinar maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
+									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
+
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. Duis ornare dictum condimentum. Vestibulum ante ipsum
+									primis in faucibus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at quam ut purus pulvinar maximus. 
+									Duis ornare dictum condimentum. Vestibulum ante ipsum primis in faucibus
+
+								</p>
+							</div>
+
+							<div class="flex mt-12 border-t-2 border-gray-200 w-full h-[40vh] lg:px-[18%] md:px-[14%] px-3">
+								<div class="lg:flex md:flex lg:flex-row md:flex-row flex flex-col lg:m-auto md:my-auto my-4 lg:my-0 md:my-0">
+									<div class="lg:h-full lg:mt-4 lg:my-auto my-auto">
+										<img class="rounded-sm object-cover h-full md:h-[25vh] lg:w-[55vw] lg:h-full md:w-[100vw] shadow-xl"src="images/proyek (1).jpg">
+									</div>
+									<div class="lg:ml-8 md:ml-8 lg:my-auto">
+										<p class="text-3xl font-bold font-secondary mt-4 mb-3 tracking-normal">Lorem ipsum dolor sit amet, consectur adipiscing elit</p>
+										<p class="text-xl font-secondary lg:my-6 my-4">Lorem ipsum dolor sit amet, consectur adipiscing elit Lorem ipsum dolor sit amet, consectur adipiscing elit</p>
+										<div class="mb-6">
+											<a href="#">
+												<div class="w-full bg-red-600 w-48 flex flex-row lg:p-4 md:p-4 p-2 flex mt-4">
+													<img src="icons/whatsapp.png" width="18" height="18" >
+													<button class="text-white mx-auto text-secondary font-semibold md:text-sm lg:text-[10px] text-[12px] hover:underline">Hubungi Kami</button>
+												</div>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+
 						</div>
 					</div>
 					</div>
