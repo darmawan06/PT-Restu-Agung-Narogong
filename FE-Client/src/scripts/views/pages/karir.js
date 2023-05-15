c1f05b82d03fca8faa1cd0e7b342fe5da94e023a
- <71787768+AmarMsd@users.noreply.github.com>
2023-05-15 22:57:21 +0700

    Update karir.js

--- FE-Client/src/scripts/views/pages/karir.js
+++ FE-Client/src/scripts/views/pages/karir.js
@@ -1,6 +1,6 @@
-import ContentData from '../../data/ContentData';
-import accessImage from '../../tools/accessImage';
-import sidebar from '../components/layouts/sidebar';
+import ContentData from "../../data/ContentData";
+import accessImage from "../../tools/accessImage";
+import sidebar from "../components/layouts/sidebar";
 const galeri = {
   async init() {
     this.contentData = new ContentData();
@@ -16,11 +16,114 @@ const galeri = {
       </div>
     </section>
     <section>
-      ISI ...................
+      <div class="w-full flex flex-col w-[90%] m-auto">
+        <div> 
+          <p class="text-xl font-bold font-primary my-4 text-xl">Web Developer</p>
+          <div class=" bg-gray-50 font-secondary p-4 text-[13px] md:text-[15px]"> 
+            <p class="font-secondary font-semibold  md:text-[18px] text-[14px]">Lorem Ipsum</p>
+            <hr class="my-4">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a nunc ipsum. Vestibulum tincidunt mollis
+              velit in posuere. Cras eu commodo dolor Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a nunc ipsum. 
+              Vestibulum tincidunt mollis velit in posuere. Cras eu commodo dolor
+            </p>
+            <button class="click_show" >Lihat Selengkapnya..</button>
+            <div class="show_text hidden">
+              <p class="font-semibold font-secondary mt-4">Requirements</p>
+              <hr class="my-4">
+              <ul class="ml-8 list-disc">
+                <li>S1</li>
+                <li>Proficient in OO web programming language, such as PHP / ASP / JSP / Ruby</li>
+                <li>Experience with MVC, Ajax, and XML is preferable</li>
+                <li>Experience with MVC, Ajax, and XML is preferable</li>
+                <li>Experience with MVC, Ajax, and XML is preferable</li>
+              </ul>
+              <form class="w-[80%] mx-auto my-10 border border-black rounded-md p-8">
+                <div class="flex flex-col">
+                  <label class="font-semibold font-secondary" for="full_name">Full Name</label>
+                  <input class="border-b border-black bg-transparent mt-2 font-secondary py-4" name="full_name" id="full_name" placeholder="Full Name">
+                </div>
+                <div class="flex flex-col mt-8">
+                  <label class="font-semibold font-secondary" for="email">E-mail Address</label>
+                  <input class="border-b border-black bg-transparent mt-2 font-secondary py-4" name="email" id="email" placeholder="Email Address">
+                </div>
+                <div class="flex flex-col mt-8">
+                  <label class="font-semibold font-secondary" for="phone_number">Phone Number</label>
+                  <input class="border-b border-black bg-transparent mt-2 font-secondary py-4" name="phone_number" id="phone_number" placeholder="Full Name">
+                </div>
+  
+                <div class="flex flex-row mt-8 font-secondary">
+                  <a class="bg-black text-white p-2 w-fit rounded-md text-[13px] md:text-[15px]" href="">Download Dokumen Persyaratan</a>
+                  <input class="ml-4 my-auto" value="file" name="Dokumen CV" type="file">
+                </div>
+  
+                <button class="bg-red-600 p-2 text-white my-8 font-semibold rounded-md" type="submit">I'M Ready To Join</button>
+  
+              </form>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="my-6"> 
+          <p class=" font-bold font-primary my-4 text-xl">Web Developer</p>
+          <div class=" bg-gray-50 font-secondary p-4 text-[13px] md:text-[15px]"> 
+            <p class="font-secondary font-semibold  md:text-[18px] text-[14px]">Lorem Ipsum</p>
+            <hr class="my-4">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a nunc ipsum. Vestibulum tincidunt mollis
+              velit in posuere. Cras eu commodo dolor Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a nunc ipsum. 
+              Vestibulum tincidunt mollis velit in posuere. Cras eu commodo dolor
+            </p>
+            <button class="click_show" >Lihat Selengkapnya..</button>
+            <div class="show_text hidden">
+              <p class="font-semibold font-secondary mt-4">Requirements</p>
+              <hr class="my-4">
+              <ul class="ml-8 list-disc">
+                <li>S1</li>
+                <li>Proficient in OO web programming language, such as PHP / ASP / JSP / Ruby</li>
+                <li>Experience with MVC, Ajax, and XML is preferable</li>
+                <li>Experience with MVC, Ajax, and XML is preferable</li>
+                <li>Experience with MVC, Ajax, and XML is preferable</li>
+              </ul>
+              <form class="w-[80%] mx-auto my-10 border border-black rounded-md p-8 ">
+                <div class="flex flex-col">
+                  <label class="font-semibold font-secondary" for="full_name">Full Name</label>
+                  <input class="border-b border-black bg-transparent mt-2 font-secondary py-4" name="full_name" id="full_name" placeholder="Full Name">
+                </div>
+                <div class="flex flex-col mt-8">
+                  <label class="font-semibold font-secondary" for="email">E-mail Address</label>
+                  <input class="border-b border-black bg-transparent mt-2 font-secondary py-4" name="email" id="email" placeholder="Email Address">
+                </div>
+                <div class="flex flex-col mt-8">
+                  <label class="font-semibold font-secondary" for="phone_number">Phone Number</label>
+                  <input class="border-b border-black bg-transparent mt-2 font-secondary py-4" name="phone_number" id="phone_number" placeholder="Full Name">
+                </div>
+  
+                <div class="flex flex-row mt-8 font-secondary">
+                  <a class="bg-black text-white p-2 w-fit rounded-md text-[13px] md:text-[15px]" href="">Download Dokumen Persyaratan</a>
+                  <input class="ml-4 my-auto" value="file" name="Dokumen CV" type="file">
+                </div>
+  
+                <button class="bg-red-600 p-2 text-white my-8 font-semibold rounded-md" type="submit">I'M Ready To Join</button>
+  
+              </form>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      
     </section>`;
   },
 
   async afterRender() {
+    $(document).ready(function () {
+      $(".click_show").click(function () {
+        $(".show_text").removeClass("hidden");
+        $(this).hide();
+      });
+    });
   },
 };
 export default galeri;
